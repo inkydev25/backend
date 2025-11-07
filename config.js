3d4b172343ddf31f29d46e1e6928562ce74784b8
inkydev25 <laroussihamid86@gmail.com>
2025-11-07 21:17:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 // config.js
 // Heure et minute du tirage en UTC
 export const SCHEDULE_HOUR = 20;
-export const SCHEDULE_MINUTE = 3;
+export const SCHEDULE_MINUTE = 22;
 
 export const SCHEDULE_DAY_OF_WEEK = 5;
+
 
 
 
