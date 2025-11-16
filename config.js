fd5c698623682199848e285c79b173729a30a095
inkydev25 <laroussihamid86@gmail.com>
2025-11-16 21:38:43 +0100

    Set withdraw time

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // config.js
 // Heure et minute du tirage en UTC
-export const SCHEDULE_HOUR = 18;
-export const SCHEDULE_MINUTE = 5;
+export const SCHEDULE_HOUR = 3;
+export const SCHEDULE_MINUTE = 0;
 
-export const SCHEDULE_DAY_OF_WEEK = 6;
+export const SCHEDULE_DAY_OF_WEEK = 5;
 
 
 
